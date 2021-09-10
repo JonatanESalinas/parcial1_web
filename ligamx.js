9cee2b3b8aee7d02aff7a977551bc809558a342c
JonatanESalinas <A01731815@itesm.mx>
2021-09-10 14:11:05 -0500

    Se agrego la funcionalidad para obtener datos de contacto
    En este commit, se agrego lo siguiente:
    -En index.html, se agrego la seccion de "Contacto" con dos inputs con sus labels,
    y un boton que invoca a una funcion del archivo ligamx.js .
    -Se agrego una funcion al archivo "ligamx.js", que manda una alert cuando se
    clickea el boton de "Suscribirme". El alert dice un mensaje con el nombre e
    email del usuario.

---
+++ ligamx.js
@@ -0,0 +1,9 @@
+function enviar_datos_contacto(){
+    var nombreElement = document.getElementById("nombre_contacto_input")
+    var emailElement = document.getElementById("email_contacto_input")
+
+    nombreUsuario = nombreElement.value;
+    emailUsuario = emailElement.value;
+
+    alert("Gracias por suscribirse, " + nombreUsuario + ".\nLe llegaran las mejores noticias de la LigaMX al correo: " + emailUsuario);
+}
