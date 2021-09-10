2f66b8156232b8d74a7434789cab0f8e2fc26a02
JonatanESalinas <A01731815@itesm.mx>
2021-09-10 14:19:26 -0500

    Se agrego seccion de contacto, con boton para suscripcion #2

--- ligamx.js
+++ ligamx.js
@@ -5,5 +5,5 @@ function enviar_datos_contacto(){
     nombreUsuario = nombreElement.value;
     emailUsuario = emailElement.value;
 
-    alert("Gracias por suscribirse, " + nombreUsuario + ".\nLe llegaran las mejores noticias de la LigaMX al correo: " + emailUsuario);
+    alert("Gracias por suscribirse, " + nombreUsuario + ".\nLe llegaran las mejores noticias de la LigaMX al correo:  " + emailUsuario);
 }
